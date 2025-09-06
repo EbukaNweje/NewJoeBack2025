5fe5d3a6dd32f9bd8f483643f6466ed7ab634073
ColdDdev <colindecorce@gmail.com>
2025-09-06 02:01:57 +0100

    D

--- controllers/depositCon.js
+++ controllers/depositCon.js
@@ -1,142 +1,136 @@
-const msgModel = require("../models/msgModel")
-const historyModel = require("../models/historyModel")
-const userModel = require("../models/User")
-const depositModel = require("../models/depositModel")
+const msgModel = require("../models/msgModel");
+const historyModel = require("../models/historyModel");
+const userModel = require("../models/User");
+const depositModel = require("../models/depositModel");
 // const currencyapi = require('@everapi/currencyapi-js');
-require("dotenv").config()
-const axios = require('axios');
-const User = require("../models/User")
+require("dotenv").config();
+const axios = require("axios");
+const User = require("../models/User");
 
 // Deposit function
 exports.deposit = async (req, res) => {
-    try {
-        // Get the depositor's id
-        const { id } = req.params;
+  try {
+    // Get the depositor's id
+    const { id } = req.params;
 
-        // Find the depositor
-        const depositor = await userModel.findById(id);
-
-        // Get the details for transaction
-        const { amount, coin } = req.body;
-        const newAmount = Number(amount);
-
-        // Check if the amount is within the allowed range
-        if (newAmount <= 0 || newAmount > 9999999 || newAmount === NaN) {
-            return res.status(400).json({
-                message: 'You can only deposit between 0 and 9,999,999'
-            });
-        }
-
-        if (coin != "BTC" && coin != "ETH") {
-            return res.status(404).json({
-                message: `Coin not available`
-            });
-        }
-
-        // Perform the currency conversion
-        let response;
-        let roundedNumber;
-
-        if (coin == "BTC") {
-            response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&precision=5`);
-            const conversionRates = response.data.bitcoin.usd;
-            const myTotal = Number(conversionRates);
-            const btcAmount = newAmount / myTotal;
-            roundedNumber = btcAmount.toFixed(9);
-        } else if (coin == "ETH") {
-            response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&precision=5`);
-            const conversionRates = response.data.ethereum.usd;
-            const myTotal = Number(conversionRates);
-            const btcAmount = newAmount / myTotal;
-            roundedNumber = btcAmount.toFixed(9);
-        }
-
-        const Depo = await depositModel.find()
-
-        // Save the deposit details
-        const deposit = new depositModel({
-            user: depositor._id,
-            amount: `${newAmount}`,
-            coin: coin,
-            total: roundedNumber,
-            status: 'pending',
-            transactionType: Depo.transactionType,
-        });
-        await deposit.save();
-
-        
-        deposit.user = id
-        // Save the transfer id to the user
-        depositor.Transactions.deposits.push(deposit._id);
-        await depositor.save();
-
-
-        if(deposit.status === 'pending'){
-            return res.status(200).json({
-                message: `Deposit made and pending`
-            })
-        }
-        if(deposit.status === 'confirmed'){
-        // Add the deposited amount to the user's account balance
-        depositor.accountBalance += newAmount;
-        }
-
-        // Create a transaction history for the depositor and save
-        const History = new historyModel({
-            id: depositor._id,
-            transactionType: deposit.transactionType,
-            amount: `${newAmount}`,
-        });
-        await History.save();
-
-        // Create a notification message for the depositor and save
-        if (deposit) {
-            const msg = `Hi ${depositor.fullName}, you just deposited ${newAmount} to your balance in ${coin}`;
-            const message = new msgModel({
-                id: depositor._id,
-                msg
-            });
-            await message.save();
-        }
-
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
-        });
+    // Find the depositor
+    const depositor = await userModel.findById(id);
+    if (!depositor) {
+      return res.status(404).json({
+        message: "User not found",
+      });
     }
-};
 
+    // Get the details for transaction
+    const { amount, coin } = req.body;
+    const newAmount = Number(amount);
+
+    // Check if the amount is within the allowed range
+    if (newAmount <= 0 || newAmount > 9999999 || isNaN(newAmount)) {
+      return res.status(400).json({
+        message: "You can only deposit between 0 and 9,999,999",
+      });
+    }
+
+    if (!["BTC", "ETH", "BNB", "DOGE", "SOL"].includes(coin)) {
+      return res.status(404).json({
+        message: `Coin not available`,
+      });
+    }
+
+    // Perform the currency conversion
+    const coinGeckoIds = {
+      BTC: "bitcoin",
+      ETH: "ethereum",
+      BNB: "binancecoin",
+      DOGE: "dogecoin",
+      SOL: "solana",
+    };
+
+    // Get current price from CoinGecko
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${coinGeckoIds[coin]}&vs_currencies=usd&precision=5`
+    );
+
+    if (!response.data || !response.data[coinGeckoIds[coin]]) {
+      return res.status(500).json({
+        message: "Error fetching cryptocurrency price",
+      });
+    }
+
+    const conversionRates = response.data[coinGeckoIds[coin]].usd;
+    const myTotal = Number(conversionRates);
+    const cryptoAmount = newAmount / myTotal;
+    const roundedNumber = cryptoAmount.toFixed(9);
+
+    // Save the deposit details
+    const deposit = new depositModel({
+      user: depositor._id,
+      amount: newAmount,
+      coin: coin,
+      total: roundedNumber,
+      status: "pending",
+      transactionType: "Deposit",
+    });
+    await deposit.save();
+
+    // Save the transfer id to the user
+    depositor.Transactions.deposits.push(deposit._id);
+    await depositor.save();
+
+    // Create a transaction history
+    const History = new historyModel({
+      id: depositor._id,
+      transactionType: "Deposit",
+      amount: newAmount,
+    });
+    await History.save();
+
+    // Create a notification message
+    const msg = `Hi ${depositor.userName}, you just deposited ${newAmount} to your balance in ${coin}`;
+    const message = new msgModel({
+      id: depositor._id,
+      msg,
+    });
+    await message.save();
+
+    return res.status(200).json({
+      message: "Deposit made and pending",
+      deposit: deposit,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 exports.getAllDeposits = async (req, res) => {
-    try {
-        // Find all deposit records and populate the user field to get user information
-        const deposits = await depositModel.find().populate('user');
+  try {
+    // Find all deposit records and populate the user field to get user information
+    const deposits = await depositModel.find().populate("user");
 
-        if (!deposits || deposits.length === 0) {
-            return res.status(404).json({
-                message: 'No deposit records found'
-            });
-        }
-
-        // Return the retrieved deposit records with user information
-        res.status(200).json({ data: deposits });
-    } catch (error) {
-        // Handle errors
-        console.error('Error fetching deposits:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    if (!deposits || deposits.length === 0) {
+      return res.status(404).json({
+        message: "No deposit records found",
+      });
     }
+
+    // Return the retrieved deposit records with user information
+    res.status(200).json({ data: deposits });
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching deposits:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-
-
 
 // // Controller function to fetch all deposits of every user
 // exports.getAllDeposits = async (req, res) => {
 
-
 // try {
 //     // Find all users
 //     const users = await userModel.find();
-
 
 //     // Create an array to store user deposits
 //     const userDeposits = [];
@@ -153,7 +147,6 @@ exports.getAllDeposits = async (req, res) => {
 //       userDeposits.push({ fullName, email, deposits });
 //     }
 //     console.log(userDeposits)
-
 
 //     // Send the array of user deposits as the response
 //     res.status(200).json({userDeposits});
--- models/depositModel.js
+++ models/depositModel.js
@@ -1,40 +1,46 @@
-const mongoose = require("mongoose")
-const {DateTime} = require('luxon')
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
-const createdOn = DateTime.now().toLocaleString({weekday:"short",month:"short",day:"2-digit", year:"numeric", hour:"2-digit",minute:"2-digit"})
-
+const createdOn = DateTime.now().toLocaleString({
+  weekday: "short",
+  month: "short",
+  day: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+});
 
 const depositSchema = new mongoose.Schema({
-    transactionType:{
-        type:String,
-        default:"Deposit"
-    },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    },
-    amount:{
-        type:Number,
-        required: true
-    },
-    coin:{
-        type:String,
-        enum: ["BTC", "ETH"]
-    },
-    total:{
-        type:Number
-    },
-    status:{
-        type:String,
-        enum: ['pending', 'confirmed'],
-        default: 'pending'
-    },
-    depositDate:{
-        type:String,
-        default:createdOn
-    }
-})
+  transactionType: {
+    type: String,
+    default: "Deposit",
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  coin: {
+    type: String,
+    enum: ["BTC", "ETH", "BNB", "DOGE", "SOL"],
+  },
+  total: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "confirmed"],
+    default: "pending",
+  },
+  depositDate: {
+    type: String,
+    default: createdOn,
+  },
+});
 
-const depositModel = mongoose.model("deposit",depositSchema)
+const depositModel = mongoose.model("deposit", depositSchema);
 
-module.exports = depositModel
+module.exports = depositModel;
